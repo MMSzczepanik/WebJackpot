b40e3baa13d4d3e52404b630353766528ac9bdc2
Michal Szczepanik <micszczepanik@gmail.com>
2019-09-10 13:14:02 +0200

    Initial Repository

---
+++ src/main/java/com/example/webjackpot/WebjackpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.webjackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjackpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebjackpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webjackpot/WebjackpotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webjackpot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebjackpotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
